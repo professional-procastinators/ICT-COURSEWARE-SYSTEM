83e8c3cafbb01d43ba838c5c73b1159166ad8534
Neil <nahidnoriel@gmail.com>
2019-02-14 12:07:24 +0800

    modified little things

--- js/cardItems.js
+++ js/cardItems.js
@@ -1,60 +1,3 @@
-var inputLessonItems = [{
-    name: "Keyboard",
-    description: "A keyboard is used to enter data into a computer or other device.",
-    imageSource: "img/keyboard-int.jpg",
-    full_description: "A computer keyboard is one of the primary input devices used with a computer. Similar to an electric typewriter, a keyboard is composed of buttons that create letters, numbers, and symbols, as well as perform other functions. A keyboard uses labeled buttons as input switches. The standard QWERTY keyboard is the commonest way to enter text and numerical data into a computer"
-},
-
-{
-    name: "Mouse",
-    description: "A mouse is a device that controls the movement of the cursor or pointer on a display screen.",
-    imageSource: "img/mouse-int.jpg",
-    full_description: "full description of mouse"
-},
-{
-    name: "Camera",
-    description: "A digital camera stores images digitally rather than recording them on film.",
-    imageSource: "img/camera-int.jpg",
-    full_description: "full description of camera",
-},
-{
-    name: "Microphone",
-    description: "A microphone is a device that captures audio by converting sound waves into an electrical signal.",
-    imageSource: "img/microphone-int.jpg",
-    full_description: "full description of microphones",
-},
-{
-    name: "Game Controller",
-    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
-    imageSource: "img/controller.jpg",
-    full_description: "full description of microphones",
-},
-{
-    name: "Scannner",
-    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
-    imageSource: "img/scanner.jpg",
-    full_description: "full description of scanner",
-},
-{
-    name: "Touch Screen",
-    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
-    imageSource: "img/touch-screen.jpg",
-    full_description: "full description of scanner",
-},
-{
-    name: "Graphics Tablet",
-    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
-    imageSource: "img/tablet.jpg",
-    full_description: "full description of scanner",
-},
-{
-    name: "Scannner",
-    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
-    imageSource: "img/scanner.jpg",
-    full_description: "full description of scanner",
-}
-];
-
 function loadCards(inputLessonItems) {
     for (var i = 0; i < inputLessonItems.length; i++) {
         // $(".container-fuild").append(`<div class="card"><div class="col-sm-4">`).append(`<div class="card">`).append(`<img class="card-img-top" src="${items[i].imageSource}" alt="Card image cap"><h4 class="card-header">Microphone</h4><div class="card-body">`).append(`<p class="card-text">${items[i].description}</p><a href="#" class="btn btn-primary">Show Lesson</a>`);
@@ -69,7 +12,7 @@ function loadCards(inputLessonItems) {
                     </div>
                 </div>`;
         $('.card-deck').append(cardString);
-
+                
         //load modal
         var modalString = `
         <div class="modal fade animated bounceInLeft" id="${i}">
---
+++ js/inputLessonItems.js
@@ -0,0 +1,59 @@
+var inputLessonItems = [{
+    name: "Keyboard",
+    description: "A keyboard is used to enter data into a computer or other device.",
+    imageSource: "img/keyboard-int.jpg",
+    full_description: "A computer keyboard is one of the primary input devices used with a computer. Similar to an electric typewriter, a keyboard is composed of buttons that create letters, numbers, and symbols, as well as perform other functions. A keyboard uses labeled buttons as input switches. The standard QWERTY keyboard is the commonest way to enter text and numerical data into a computer"
+},
+
+{
+    name: "Mouse",
+    description: "A mouse is a device that controls the movement of the cursor or pointer on a display screen.",
+    imageSource: "img/mouse-int.jpg",
+    full_description: "full description of mouse"
+},
+{
+    name: "Camera",
+    description: "A digital camera stores images digitally rather than recording them on film.",
+    imageSource: "img/camera-int.jpg",
+    full_description: "full description of camera",
+},
+{
+    name: "Microphone",
+    description: "A microphone is a device that captures audio by converting sound waves into an electrical signal.",
+    imageSource: "img/microphone-int.jpg",
+    full_description: "full description of microphones",
+},
+{
+    name: "Game Controller",
+    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
+    imageSource: "img/controller.jpg",
+    full_description: "full description of microphones",
+},
+{
+    name: "Scannner",
+    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
+    imageSource: "img/scanner.jpg",
+    full_description: "full description of scanner",
+},
+{
+    name: "Touch Screen",
+    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
+    imageSource: "img/touch-screen.jpg",
+    full_description: "full description of scanner",
+},
+{
+    name: "Graphics Tablet",
+    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
+    imageSource: "img/tablet.jpg",
+    full_description: "full description of scanner",
+},
+{
+    name: "Scannner",
+    description: "A game controller is a peripheral device designed to be connected to a computer or console gaming system.",
+    imageSource: "img/scanner.jpg",
+    full_description: "full description of scanner",
+}
+];
+var outLessonItems = [{
+
+}];
