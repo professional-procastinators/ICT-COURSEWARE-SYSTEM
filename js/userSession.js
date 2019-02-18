42cbbee25629fe2e8cc907a504762707fe8645b9
kathmeooow <36495572+kathmeooow@users.noreply.github.com>
2019-02-18 14:57:23 +0800

    updates updates updates

--- js/userSession.js
+++ js/userSession.js
@@ -1,7 +1,9 @@
 class Lesson{
-    constructor(lessonsIDArray){
+    constructor(lessonsIDArray, lessonName){
         this.hasWatchedVid = false;
         this.lessonsMap = Lesson.setUpLessonsMap(lessonsIDArray);
+        this.lessonName = lessonName;
+        Lesson.setUpLessonsButtonEvents(this);
     }
 
     static setUpLessonsMap(lessonsIDArray){
@@ -14,37 +16,54 @@ class Lesson{
         lessonsMap.set('haveFinishedAll','done');
         return lessonsMap;
     }
-}
-let inputLessonNames = [];
-inputLessonItems.forEach((item)=>{inputLessonNames.push(item.name);console.log(item.name);});
-let inputLesson = new Lesson(inputLessonNames);
-$(document).ready(()=>{
-    console.log("ready");
-    $(`[id='btn-vid']`).click(()=>{
-        console.log("clicked on watch vid");
-        inputLesson.lessonsMap.set('hasWatchedVid',true);
-        if(!Array.from(inputLesson.lessonsMap.values()).includes(false)){
-            inputLesson.lessonsMap.set('haveFinishedAll',true);
-        }
-        //store the inputLessonMap to localStorage
-        localStorage.setItem("inputLessonMap",JSON.stringify(Array.from(inputLesson.lessonsMap.entries())));
-    });
-    for (let key of inputLesson.lessonsMap.keys()) {
-
-        //sets up each key button event to update to true once the user clicked on the lesson
-        //key = Lesson name ex: Mouse, Keyboard
+    static setUpLessonsButtonEvents(lesson){
+        $(document).ready(()=>{
+            console.log("ready");
+            $(`[id='btn-vid']`).click(()=>{
+                console.log("clicked on watch vid");
+                lesson.lessonsMap.set('hasWatchedVid',true);
+                if(!Array.from(lesson.lessonsMap.values()).includes(false)){
+                    lesson.lessonsMap.set('haveFinishedAll',true);
+                }
+                //store the inputLessonMap to localStorage
+                localStorage.setItem(`${lesson.lessonName}`,JSON.stringify(Array.from(lesson.lessonsMap.entries())));
+            });
+            for (let key of lesson.lessonsMap.keys()) {
         
-        $(`[id='${key}']`).click(()=>{
-            console.log(`${key} was set to true`);
-            inputLesson.lessonsMap.set(key,true);
-            //lesson quiz is accessible once all the lessons have been watched
-             if(!Array.from(inputLesson.lessonsMap.values()).includes(false)){
-                 inputLesson.lessonsMap.set('haveFinishedAll',true);
-             }
-             //local storage will be set everytime a lesson button is clicked
-             localStorage.setItem("inputLessonMap",JSON.stringify(Array.from(inputLesson.lessonsMap.entries())));
-            // console.log(localStorage.getItem('inputLessonMap'));
-            console.log(inputLesson.lessonsMap);
+                //sets up each key button event to update to true once the user clicked on the lesson
+                //key = Lesson name ex: Mouse, Keyboard
+                
+                $(`[id='${key}']`).click(()=>{
+                    console.log(`${key} was set to true`);
+                    lesson.lessonsMap.set(key,true);
+                    //lesson quiz is accessible once all the lessons have been watched
+                     if(!Array.from(lesson.lessonsMap.values()).includes(false)){
+                         lesson.lessonsMap.set('haveFinishedAll',true);
+                     }
+                     //local storage will be set everytime a lesson button is clicked
+                     localStorage.setItem(`${lesson.lessonName}`,JSON.stringify(Array.from(lesson.lessonsMap.entries())));
+                    // console.log(localStorage.getItem('inputLessonMap'));
+                    console.log(lesson.lessonsMap);
+                });
+            }
         });
     }
-});
+}
+
+//set up inputLessonMapSession
+if(localStorage.getItem("inputLessonsMap")===null){
+    let inputLessonNames = [];
+    inputLessonItems.forEach((item)=>{inputLessonNames.push(item.name);console.log(item.name);});
+    inputLesson = new Lesson(inputLessonNames,"inputLessonsMap");
+}
+if(localStorage.getItem("outputLessonsMap")===null){
+    let inputLessonNames = [];
+    inputLessonItems.forEach((item)=>{inputLessonNames.push(item.name);console.log(item.name);});
+    inputLesson = new Lesson(inputLessonNames,"outputLessonsMap");
+}
+if(localStorage.getItem("storageLessonsMap")===null){
+    let inputLessonNames = [];
+    inputLessonItems.forEach((item)=>{inputLessonNames.push(item.name);console.log(item.name);});
+    inputLesson = new Lesson(inputLessonNames,"storageLessonsMap");
+}
+
