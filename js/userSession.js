48aa7c2d871bd3c6aa2f9e9b6ae0f9df971c3b88
Neil <nahidnoriel@gmail.com>
2019-02-19 13:32:08 +0800

    added some bugfixes

--- js/userSession.js
+++ js/userSession.js
@@ -23,7 +23,13 @@ class Lesson{
                 console.log("clicked on watch vid");
                 lesson.lessonsMap.set('hasWatchedVid',true);
                 if(!Array.from(lesson.lessonsMap.values()).includes(false)){
+                    console.log("successfully finished lesson");
                     lesson.lessonsMap.set('haveFinishedAll',true);
+                    Swal.fire({
+                        title: `Unlocked!`,
+                        text: `Congratulations! You've completed the Input Device Lesson! You've unloced the quiz for Input Devices.`,
+                        type: 'info'
+                    });
                 }
                 //store the inputLessonMap to localStorage
                 localStorage.setItem(`${lesson.lessonName}`,JSON.stringify(Array.from(lesson.lessonsMap.entries())));
@@ -38,6 +44,7 @@ class Lesson{
                     lesson.lessonsMap.set(key,true);
                     //lesson quiz is accessible once all the lessons have been watched
                      if(!Array.from(lesson.lessonsMap.values()).includes(false)){
+                        console.log("successfully finished lesson");
                          lesson.lessonsMap.set('haveFinishedAll',true);
                          Swal.fire({
                             title: `Unlocked!`,
@@ -56,19 +63,34 @@ class Lesson{
 }
 
 //set up inputLessonMapSession
-if(localStorage.getItem("inputLessonsMap")===null || inputLessonItems===null || undefined){
+var url_string = window.location.href //window.location.href
+var url = new URL(url_string);
+let key1 = url.searchParams.get("key1");
+if(key1==="0"){
     let inputLessonNames = [];
     inputLessonItems.forEach((item)=>{inputLessonNames.push(item.name);console.log(item.name);});
     inputLesson = new Lesson(inputLessonNames,"inputLessonsMap");
+    if(localStorage.getItem("inputLessonsMap")!==null){
+        console.log("you just went here")
+        inputLesson.lessonsMap = JSON.parse(localStorage.getItem("inputLessonsMap"));
+    }
 }
-if(localStorage.getItem("outputLessonsMap")===null && inputLessonItems===null || undefined){
+if(key1==="1"){
     let outputLessonNames = [];
     outputLessonItems.forEach((item)=>{storageLessonName.push(item.name);console.log(item.name);});
     outputLesson = new Lesson(outputLessonNames,"outputLessonsMap");
+    if(localStorage.getItem("outputLessonsMap")!==null){
+        console.log("you just went here")
+        outputLesson.lessonsMap = JSON.parse(localStorage.getItem("outputLessonsMap"));
+    }
 }
-if(localStorage.getItem("storageLessonsMap")===null && inputLessonItems===null || undefined){
+if(key1==="2"){
     let storageLessonName = [];
     storageLessonItems.forEach((item)=>{storageLessonName.push(item.name);console.log(item.name);});
     storageLesson = new Lesson(storageLessonName,"storageLessonsMap");
+    if(localStorage.getItem("storageLessonsMap")!==null){
+        console.log("you just went here")
+        storageLesson.lessonsMap = JSON.parse(localStorage.getItem("storageLessonMap"));
+    }
 }
 
