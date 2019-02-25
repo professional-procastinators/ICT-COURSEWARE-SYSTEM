c1d7c20bf23889b792e95cb004d39fe4a37893d2
kathmeooow <36495572+kathmeooow@users.noreply.github.com>
2019-02-25 23:02:35 +0800

    added videos and change html size

--- alima/login_module.bat
+++
@@ -1,9 +0,0 @@
-:registration
-set /p newUsername= Enter your new username:
-set /p newPassword= Enter your new password:
-echo Registration Successful!
-echo fuck this shit bruh
-
-:loginWindow
-echo Welcome
-echo if newUsername = 
